2ef757262433df4bda854b9d0f3243f365017a11
David Ryskalczyk <david@rysk.us>
2024-02-10 18:50:01 -0500

    Reduce windows.h defines

--- src/ngscopeclient/PreferenceManager.cpp
+++ src/ngscopeclient/PreferenceManager.cpp
@@ -34,6 +34,8 @@
 #include <stdexcept>
 
 #ifdef _WIN32
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <shlwapi.h>
 #include <shlobj.h>
--- src/ngscopeclient/Session.cpp
+++ src/ngscopeclient/Session.cpp
@@ -52,6 +52,8 @@
 #include <cinttypes>
 
 #ifdef _WIN32
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <shlwapi.h>
 #else
